f0dc935e14e7f19e755c89cba814652a852667e3
rohanmittal <rohanmittal@gmail.com>
2021-02-24 13:35:41 +0530

    initial commit

---
+++ register.php
@@ -0,0 +1,13 @@
+<?php
+    $connection=mysqli_connect("localhost","root","","lms");
+    $db=mysqli_select_db($connnection,"lms");
+    // if($connection)echo "Database is connected";
+    // else echo"Problem Occured";
+
+    $query="insert into users values(null,'$_POST[name]','$_POST[email]','$_POST[password]',$_POST[mobile],'$_POST[address]')";
+    $mysql_run=mysqli_query($connection,$query);
+?>
+<script type="text/javascript">
+    alert("Registration is successfull ... You may login now.")
+    //window.location.href="index.php";
+</script>
---
+++ signup.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <!-- <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href=""> -->
+
+    <meta charset="utf-8" name="viewpoint" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="bootstrap-5.0.0-beta2-dist/css\bootstrap.min.css">
+    <script type="text/javascript" src="bootstrap-5.0.0-beta2-dist/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        #side_bar
+        {
+            background-color: whitesmoke;
+            padding: 50px;
+            width : 300px;
+            height:450 px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Library Management System(LMS)</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin/index.php">Admin Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">User Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.php">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav><br>
+
+    <span>
+        <marquee> This is Library Management System. </marquee>
+    </span><br>
+
+    <div class="row">
+
+        <div class="col-md-4" id="side_bar">
+            <h5>Library Timing</h5>
+            <ul>
+
+                <li>Opening Timing:8:00 AM</li> 
+                <li>Closing Timing:8:00 PM</li> 
+                <li>(Sunday Off)</li>    
+            </ul>
+            <h5>What we Provide</h5>
+            <ul>
+
+                <li>Full Ferniture</li> 
+                <li>Free Wi-Fi</li> 
+                <li>News Paper</li>
+                <li>Discussion Room</li> 
+                <li>RO Water</li> 
+                <li>Peacefull Environment</li>    
+            </ul>
+        </div>
+        <div class="col-md-8" id="main_content">
+            <center><h3>User Registration</h3></center>
+            <form action ="register.php" method="post">
+            <div class="form-group">
+                <label for ="name">Full Name:</label>
+                <input type="text" name="name" class="form-control" required>
+
+                <label for ="name">Email ID:</label>
+                <input type="text" name="email" class="form-control" required>
+
+                <label for ="name">Password:</label>
+                <input type="password" name="password" class="form-control" required>
+
+                <label for ="name">Mobile Number:</label>
+                <input type="text" name="mobile" class="form-control" required>
+
+                <label for ="name">Address:</label>
+                <textarea rows="3" cols="40" class="form-control" name="address"></textarea>
+
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+            </form>
+        </div>
+    </div>
+
+    
+
+</body>
+
+</html>
